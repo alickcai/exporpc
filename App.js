b21f96f81c4146a844c0df4c7921de992cedab48
Robin <10628169+tapestryonly@users.noreply.github.com>
2024-03-15 17:38:44 +0800

    Upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function App() {
+  return (
+    <View style={{ flex: 1, flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 32, color: "black" }}>{"Welcome"}</Text>
+    </View>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,13 @@
+# Instruction on Master
+
+## Available Branches
+
+In the directory, you can find and access below branches:
+
+### `release`
+
+Branch on planned release.
+
+### `hotfix`
+
+Branch on production bug-fix.
